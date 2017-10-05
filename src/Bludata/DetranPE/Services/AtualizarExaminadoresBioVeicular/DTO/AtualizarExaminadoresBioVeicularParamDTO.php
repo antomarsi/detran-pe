d3d23ed067c781eba3fdc17f3061f89a3ef7f9ad
Antonio Marco da Silva <antomarsi@hotmail.com>
2017-10-05 17:22:56 +0000

    Fix de pitocagem, ajustado a order dos fields

--- src/Bludata/DetranPE/Services/AtualizarExaminadoresBioVeicular/DTO/AtualizarExaminadoresBioVeicularParamDTO.php
+++ src/Bludata/DetranPE/Services/AtualizarExaminadoresBioVeicular/DTO/AtualizarExaminadoresBioVeicularParamDTO.php
@@ -14,14 +14,14 @@ final class AtualizarExaminadoresBioVeicularParamDTO extends DTO
     /**
      * Código do examinador.
      *
-     * @Field(name="nExaminador", order="", type="integer")
+     * @Field(name="nExaminador", order=1, type="integer")
      */
     protected $examinador;
 
     /**
      * Ação a ser realizada no cadastro do examinador.
      *
-     * @Field(name="sAcao", order="", type="string")
+     * @Field(name="sAcao", order=0, type="string")
      */
     protected $acao;
 }
